f6f8ece55347799e62b27c2afc07c1f2dd1c60f6
guangyue.zhang <guangyue.zhang@softbankrobotics.com>
2018-12-17 18:54:13 +0900

    almost ?

--- entry_game.py
+++ entry_game.py
@@ -1,31 +1,30 @@
-from service import bank_account
+from service.player import Player
 from service.dealer import Dealer
 from colorama import Fore, Style
+from service import common_util
 
 
 class Black_Jack():
     def __init__(self):
         print('Welcome to Black Jack!')
-        self.player_bankroll = bank_account.Bank_Account(1000)
-        print('Your initial bankroll is: ' + self.player_bankroll)
+        self.player = Player(1000)
+        print('Your initial bankroll is: ' + self.player.balance)
         while True:
-            self.player_curr_bet = input('Please input your first bet : ')
-            if self.player_bankroll.withdraw(self.player_curr_bet):
+            self.player.curr_bet = input('Please input your first bet : ')
+            if self.player.withdraw(self.player.curr_bet):
                 break
-        print('Your current bet: ' + self.player_curr_bet)
+        print('Your current bet: ' + self.player.curr_bet)
         self.dealer = Dealer()
-        self.player_cards = []
-        self.dealer_cards = []
-        self.player_cards.append(self.dealer.deal())
-        self.player_cards.append(self.dealer.deal())
-        self.dealer_cards.append(self.dealer.deal())
-        self.dealer_cards.append(self.dealer.deal())
-        self.print_cards('first'=True)
+        self.hit('player')
+        self.hit('player')
+        self.hit('dealer')
+        self.hit('dealer', hide=True)
+        self.print_cards(first=True)
         self.gambling()
 
-    def print_cards(self, first=false):
-        print('Your card : ' + Fore.RED + str(self.player_cards) + Style.RESET_ALL)
-        print("Dealer's card : " + Fore.BLUE + str((['*'] + self.dealer_cards[1:]) if first else self.dealer_cards))
+    def print_cards(self, first=False):
+        print('Your card : ' + Fore.RED + str(self.player.hand_cards) + Style.RESET_ALL)
+        print("Dealer's card : " + Fore.BLUE + str((['*'] + self.dealer.hand_cards[1:]) if first else self.dealer.hand_cards))
 
     def gambling(self):
         while not self.game_over():
@@ -41,21 +40,51 @@ class Black_Jack():
         print(Style.RESET_ALL)
         while True:
             self.player_choice = input('Please input your choice (1 or 2): ')
-            if self.player_choice != '1' and self.player_choice != '2':
+            if not common_util.check_int(self.player_choice) or (self.player_choice != '1' and self.player_choice != '2'):
                 print('Please input 1 or 2 and try again')
             else:
                 break
 
-    def hit(player):
+    def hit(self, player, hide=False):
         while not self.game_over():
-            if player == 'dealer':
-                self.dealer_cards.append(self.dealer.deal())
-            else:
-                self.player_cards.append(self.dealer.deal())
+            self.new_card = self.dealer.deal()
+            self.sum_cards(player)
+            if not hide:
+                print(player + ' new card : ' + str(self.new_card))
+                print(player + ' new point : ' + str(self.dealer.sum_point if player == 'dealer' else self.player.sum_point))
 
-    def game_over():
-        # sum point of the player and the dealer's cards
-        player_cards_copy = self.player_cards;
-        for card in player_cards_copy:
-            if 
-        # check if there is a winner
+    '''check if there is a winner'''
+
+    def game_over(self):
+        if self.dealer.sum_point > 21:
+            print('You Win! Dealer is busted! Game Over!')
+            return True
+        elif self.player.sum_point > 21:
+            print('You Lost! You are busted! Game Over!')
+            return True
+        elif self.dealer.sum_point > self.player.sum_point:
+            print('You Lost! Dealer won! Game Over!')
+            return True
+        else:
+            return False
+
+    '''sum point of the player and the dealer's cards'''
+
+    def sum_cards(self, player):
+        new_card = self.new_card
+        sum_point = self.dealer.sum_point if player == 'dealer' else self.player.sum_point
+        if common_util.check_int(new_card):
+            new_point = int(new_card)
+        elif new_card in ['J', 'Q', 'K']:
+            new_point = 10
+        elif abs(sum_point + 1 - 21) < abs(sum_point + 11 - 21):
+            new_point = 1
+        else:
+            new_point = 11
+        sum_point = sum_point + new_point
+        if player == 'dealer':
+            self.dealer.sum_point = sum_point
+            self.dealer.hand_cards.append(new_card)
+        else:
+            self.player.sum_point = sum_point
+            self.player.hand_cards.append(new_card)
--- service/dealer.py
+++ service/dealer.py
@@ -1,11 +1,15 @@
 from random import shuffle
+
+
 class Dealer():
-    cards = []
+    card_decks = []
+    hand_cards = []
+    sum_point = 0
 
     def __init__(self):
-        self.cards = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']*3
-        print('Cards being initialized!')
-        shuffle(self.cards)
+        Dealer.card_decks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'] * 3
+        print('Card decks being initialized!')
+        shuffle(Dealer.card_decks)
 
     def deal(self):
-        return self.cards.pop()
+        return Dealer.card_decks.pop()
