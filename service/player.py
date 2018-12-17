618e0ed800a8d8bfc36ffc5dc5b88e1743d45a8e
guangyue.zhang <guangyue.zhang@softbankrobotics.com>
2018-12-17 18:54:13 +0900

    almost ?

--- service/player.py
+++ service/player.py
@@ -1,22 +1,24 @@
 from service import common_util
 
 
-class Bank_Account():
+class Player():
     def __init__(self, balance=0):
         self.balance = 0
+        self.curr_bet = 0
+        self.hand_cards = []
+        self.sum_point = 0
 
     def deposite(self, money):
-        if int(money) and money > 0:
-            self.balance = self.balance + money
+        if common_util.check_int(money) and int(money) > 0:
+            self.balance = self.balance + int(money)
             return True
         else:
+            print('Incorrect money number!')
             return False
 
     def withdraw(self, money):
-        if not common_util.check_int(money):
-            return False
-        if int(money) and money > 0:
-            self.balance = self.balance - money
+        if common_util.check_int(money) and int(money) > 0:
+            self.balance = self.balance - int(money)
             if self.balance < 0:
                 print('Your balance is insufficient，only ' + str(self.balance) + ' left.')
                 return False
